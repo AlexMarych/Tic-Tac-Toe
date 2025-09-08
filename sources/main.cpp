b9cf8883a9e1df20268d6ff69fca9e56283e1a86
AlexMarych <alexander.marych@gmail.com>
2025-09-08 20:04:22 +0200

    Init commit

---
+++ sources/main.cpp
@@ -0,0 +1,20 @@
+#include <raylib.h>
+#include <raygui.h>
+
+int main() {
+	
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	InitWindow(screenWidth, screenHeight, "Angry Pirates");
+	SetTargetFPS(60);             
+
+	while (!WindowShouldClose())   
+	{
+		
+
+	}
+	
+	CloseWindow();
+	
+	return 1;
+}
