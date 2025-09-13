e4a292586b2a89373b4998c531b68e58e560e50b
AlexMarych <alexander.marych@gmail.com>
2025-09-13 22:01:35 +0200

    Main game loop set up

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,20 +1,39 @@
 #include <raylib.h>
 #include <raygui.h>
+#include "Constants.hpp"
+#include "scene/manager/SceneManger.hpp"
+#include <scene/LevelScene.hpp>
+#include <memory>
+
 
 int main() {
 	
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-	InitWindow(screenWidth, screenHeight, "Angry Pirates");
-	SetTargetFPS(60);             
+	InitWindow(
+		AppConstants::ScreenWidth,
+		AppConstants::ScreenHeight,
+		AppConstants::WindowTitle.c_str()
+	);
+
+	auto levelScene1 = new BaseScene();
+	levelScene1->setName("Level 1");
+
+
+	auto sceneManager = new SceneManager(levelScene1);
+
+
+	SetTargetFPS(AppConstants::TargetFPS);             
+
+	
 
 	while (!WindowShouldClose())   
 	{
-		
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+	
 
+		EndDrawing();
 	}
-	
+
 	CloseWindow();
-	
-	return 1;
+	return 0;
 }
