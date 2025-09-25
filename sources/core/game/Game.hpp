92ad291092b82865cbc2af3fbcdcacba3cd52deb
AlexMarych <alexander.marych@gmail.com>
2025-09-25 23:52:04 +0200

    Game class added

---
+++ sources/core/game/Game.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <raylib.h>
+#include "scene/manager/SceneManger.hpp"
+
+namespace Core {
+
+	struct GameConfig
+	{
+		int SCREEN_WIDTH = 800;
+		int SCREEN_HEIGHT = 600;
+		int FPS = 60;
+		std::string WINDOW_TITLE = "Game";
+	};
+
+	class Game
+	{
+	private: 
+		bool isRunning;
+		GameConfig config;
+		SceneManager* sceneManager;
+
+	public:
+		Game(const GameConfig& config) : config(config) {
+			SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+			InitWindow(config.SCREEN_WIDTH, config.SCREEN_HEIGHT, config.WINDOW_TITLE.c_str());
+			SetTargetFPS(config.FPS);
+			sceneManager = new SceneManager();
+		}
+
+		~Game() {
+			CloseWindow();
+		}
+
+		
+		void Stop() {
+			isRunning = false;
+		}
+
+		void Run() {
+			isRunning = true;
+
+			while (isRunning) {
+
+				if (WindowShouldClose()) {
+					Stop();
+					break;
+				}
+
+				BeginDrawing();
+				ClearBackground(RAYWHITE);
+				
+				sceneManager->update(GetFrameTime());
+
+				EndDrawing();
+			}
+		}
+	};
+
+}
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,40 +1,11 @@
-#include <raylib.h>
-#include <raygui.h>
-#include "Constants.hpp"
-#include "scene/manager/SceneManger.hpp"
-#include <scene/LevelScene.hpp>
-#include <memory>
+#include "core/game/Game.hpp"
 
 
 int main() {
 	
-	InitWindow(
-		AppConstants::ScreenWidth,
-		AppConstants::ScreenHeight,
-		AppConstants::WindowTitle.c_str()
-	);
+	Core::GameConfig config;
+	config.WINDOW_TITLE = "Angry Pirates";
 
-	auto levelScene1 = new BaseScene();
-	levelScene1->setName("Level 1");
-
-
-	auto sceneManager = new SceneManager(levelScene1);
-
-
-	SetTargetFPS(AppConstants::TargetFPS);             
-
-	
-
-	while (!WindowShouldClose())   
-	{
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-	
-		sceneManager->update(GetFrameTime());
-
-		EndDrawing();
-	}
-
-	CloseWindow();
-	return 0;
+	Core::Game game(config);
+	game.Run();
 }
