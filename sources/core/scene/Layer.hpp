26a7f1dfddee26e46844393df1265105265a36ad
AlexMarych <alexander.marych@gmail.com>
2025-10-18 23:59:24 +0200

    Scene system refactored

--- sources/core/scene/Layer.hpp
+++ sources/core/scene/Layer.hpp
@@ -16,7 +16,6 @@ namespace Scene
 		
 		std::vector<std::shared_ptr<Core::IRenderable>> renderObjects;
 		std::vector<std::shared_ptr<Core::IUpdatable>> updateObjects;
-
 		
 	public:
 		Layer() = default;
@@ -26,26 +25,56 @@ namespace Scene
 
 		virtual void update(float deltaTime) override
 		{
-			for (const auto& obj : updateObjects) {
-				obj->update(deltaTime);
+			for (const auto& obj : updateObjects) 
+			{
+				if (obj) obj->update(deltaTime);
 			}
 		}
 
 		virtual void render() override
 		{
-			for (const auto& obj : renderObjects) {
-				obj->render();
+			for (const auto& obj : renderObjects) 
+			{
+				if (obj) obj->render();
 			}
 		}
 
 		template <typename T>
 		void addObject(const std::shared_ptr<T>& obj) {
+
+			if (!obj) return;
+
 			if (auto u = std::dynamic_pointer_cast<IUpdatable>(obj)) {
 				updateObjects.push_back(u);
 			}
 			if (auto r = std::dynamic_pointer_cast<Renderable>(obj)) {
 				renderObjects.push_back(r);
 			}
+		}
+
+		void addUpdatable(const std::shared_ptr<Core::IUpdatable>& obj)
+		{
+			if (obj) updateObjects.push_back(obj);
+		}
+
+		void addRenderable(const std::shared_ptr<Core::IRenderable>& obj)
+		{
+			if (obj) renderObjects.push_back(obj);
+		}
+
+		template <typename T>
+		void removeObject(const std::shared_ptr<T>& obj)
+		{
+			if (!obj) return;
+			removeFromList(updateObjects_, obj);
+			removeFromList(renderObjects_, obj);
+		}
+
+		
+		void clear() noexcept
+		{
+			updateObjects.clear();
+			renderObjects.clear();
 		}
 
 		virtual void LoadFromJson(const nlohmann::json& data) override
