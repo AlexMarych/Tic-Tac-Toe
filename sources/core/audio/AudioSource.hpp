3b96e871ed980417f0eefa966a408a18cf91bd69
AlexMarych <alexander.marych@gmail.com>
2025-10-18 23:58:00 +0200

    Audio system refactored

--- sources/core/audio/AudioSource.hpp
+++ sources/core/audio/AudioSource.hpp
@@ -2,47 +2,102 @@
 
 #include "raylib.h"
 #include <string>
-#include <iostream>
+#include "core/utils/DebugUtils.hpp"
 
 namespace Audio
 {
-	class AudioSource
-	{
-	private:
-		Sound sound;
-		bool isLoaded = false;
+class AudioSource
+{
+private:
+    Sound sound{};
+    bool loaded{false};
 
-	public:
-		AudioSource() {}
-		virtual ~AudioSource() = default;
+public:
+    AudioSource() noexcept = default;
+    virtual ~AudioSource() noexcept { unload(); }
 
-		static bool LoadFromFile(const char* filePath, AudioSource* source)
-		{
-			if (source->isLoaded) source->Unload();
+    AudioSource(const AudioSource&) = delete;
+    AudioSource& operator=(const AudioSource&) = delete;
 
-			std::string fullPath = std::string(ASSETS_PATH) + filePath;
-			source->sound = LoadSound(fullPath.c_str());
-			source->isLoaded = true;
-			return source->isLoaded;
-		}
+    AudioSource(AudioSource&& other) noexcept
+        : sound(other.sound), loaded(other.loaded)
+    {
+        other.sound = {};
+        other.loaded = false;
+    }
 
-		virtual void Unload() {
-			this->Stop();
-			if (isLoaded) {
-				UnloadSound(this->sound);
-				isLoaded = false;
-			}
-		}
+    AudioSource& operator=(AudioSource&& other) noexcept
+    {
+        if (this != &other) {
+            unload();
+            sound = other.sound;
+            loaded = other.loaded;
+            other.sound = {};
+            other.loaded = false;
+        }
+        return *this;
+    }
 
-		virtual void Play() 
-		{ 
-			std::cout << "Playing sound" << std::endl;
-			PlaySound(this->sound); 
-		}
-		virtual void Pause() { if (IsPlaying()) PauseSound(this->sound); }
-		virtual void Resume() { if (!IsPlaying()) ResumeSound(this->sound); }
-		virtual void Stop() { if(IsPlaying()) StopSound(this->sound);}
-		virtual bool IsPlaying() const { return IsSoundPlaying(sound); };
-		
-	};
-}
+    
+    bool loadFromFile(const std::string& filePath)
+    {
+        if (loaded) unload();
+        std::string fullPath = std::string(ASSETS_PATH) + filePath;
+        sound = LoadSound(fullPath.c_str());
+        loaded = (sound.frameCount != 0 || sound.stream != 0);
+        if (!loaded) {
+            DebugUtils::println(std::string("AudioSource: failed to load '") + fullPath + "'");
+        }
+        return loaded;
+    }
+
+    static AudioSource createFromFile(const std::string& filePath)
+    {
+        AudioSource src;
+        src.loadFromFile(filePath);
+        return src;
+    }
+
+    void unload() noexcept
+    {
+        if (!loaded) return;
+        stop();
+        UnloadSound(sound);
+        sound = {};
+        loaded = false;
+    }
+
+    void play()
+    {
+        if (!loaded) {
+            DebugUtils::println("AudioSource::play: sound not loaded");
+            return;
+        }
+        DebugUtils::println("AudioSource: play");
+        PlaySound(sound);
+    }
+
+    void pause()
+    {
+        if (isPlaying()) PauseSound(sound);
+    }
+
+    void resume()
+    {
+        if (!isPlaying() && loaded) ResumeSound(sound);
+    }
+
+    void stop()
+    {
+        if (isPlaying()) StopSound(sound);
+    }
+
+    bool isPlaying() const
+    {
+        if (!loaded) return false;
+        return IsSoundPlaying(sound);
+    }
+
+    bool isLoaded() const noexcept { return loaded; }
+};
+} 
